910e6387f2f4185bfc05fdcc9007aa7538f3a2a2
mateoszsz <mateuszszmal@gmail.com>
2020-05-15 16:12:50 +0200

    ''

---
+++ Arrays_and_Strings/1_6StringCompression.py
@@ -0,0 +1,41 @@
+import unittest
+
+def Compression(string):
+    if len(string) == 0:
+        return -1
+    output = ''
+    previous = None
+    count = 0
+    for current in string:
+        if previous:
+            if previous == current:
+                count+=1
+            else:
+                output += previous
+                output += str(count)
+                previous = current
+                count = 1
+        else:
+            previous = current
+            count = 1
+    output += previous
+    output += str(count)
+    if len(output) < len(string):
+        return output
+    return string
+
+
+class tests (unittest.TestCase):
+    data =[
+        ('aabcccccaaa','a2blc5a3'),
+        ('',-1),
+        ('abcdef','abcdef')
+    ]
+
+    def trueTest(self):
+        for [input,expected] in self.data:
+            output = Compression(input)
+            self.assertEqual(output, expected)
+
+if __name__ == '__main__':
+    unittest.main()
